c525b4567dc36a3f6351ca96c1d99c3b3c0779f3
Jim Crossley <jim@crossleys.org>
2013-05-18 22:31:43 -0400

    Update jobs to reflect recent changes

--- src/demo/jobs.clj
+++ src/demo/jobs.clj
@@ -2,16 +2,39 @@
   (:require [immutant.jobs :as jobs])
   (:import java.util.Date))
 
-(jobs/schedule "every-5s-forever"  
-               #(println (str (Date.)))
-               "*/5 * * * * ?"
+;;; Strings are military time
+(jobs/schedule "run-reports"
+               #(println "reporting")
+               :at "0400" :every :day)
+
+;;; java.util.Date, possibly returned by clj-time
+(jobs/schedule "run-reports"
+               #(println "reporting")
+               :at (Date.) :every :day)
+
+;;; Remember the 0's!
+(jobs/schedule "nothing-at-9am"  
+               #(println "nothing")
+               "0 0 9 * * ?"
                :singleton false)
 
-(jobs/schedule "4-times-every-10ms-in-500ms"  
+;;; Fancy cron spec feature
+(jobs/schedule "every-5s-forever"  
                #(println (Date.))
+               "*/5 * * * * ?")
+
+(defn foo [] (println (Date.) "foo"))
+
+;;; A period spec is a vector of a number and keyword
+(jobs/schedule "every-5s-forever"  
+               #'foo
+               :every [5 :seconds])
+
+;;; Numbers are milliseconds
+(jobs/schedule "4-times-every-10ms-in-500ms"  
+               #(println (Date.) "test")
                :in     500
                :every  10
                :repeat 3)
 
 (jobs/unschedule "every-5s-forever")
-(jobs/unschedule "4-times-every-10ms-in-500ms")
