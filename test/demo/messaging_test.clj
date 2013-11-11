5cfe1652306072d7bb33a3559d95c096f857b60a
Jim Crossley <jim@crossleys.org>
2013-11-11 18:43:49 -0500

    Add a better integration test

---
+++ test/demo/messaging_test.clj
@@ -0,0 +1,14 @@
+(ns demo.messaging-test
+  (:use clojure.test
+        immutant.messaging))
+
+(deftest ^:integration listening
+  (start "test.topic")
+  (let [expected (promise)
+        handler (listen "test.topic" #(deliver expected %))]
+    (try
+      (publish "test.topic" :success)
+      (is (= :success (deref expected 500 :fail)))
+      (finally
+       @(unlisten handler)
+       (stop "test.topic")))))
