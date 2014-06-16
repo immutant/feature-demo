68d04a796148cb4a7230768cfd5e82606e386620
Toby Crawley <toby@tcrawley.org>
2014-06-16 14:00:09 -0400

    Run messaging as part of core/-main.

--- src/demo/core.clj
+++ src/demo/core.clj
@@ -1,7 +1,9 @@
 (ns demo.core
   (:require demo.web
-            demo.scheduling))
+            demo.scheduling
+            demo.messaging))
 
 (defn -main [& args]
   (apply demo.web/-main args)
-  (apply demo.scheduling/-main args))
+  (apply demo.scheduling/-main args)
+  (apply demo.messaging/-main args))
