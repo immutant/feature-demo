8b38cd7d6c78f88c4937d33fcee19f956a4f3548
Toby Crawley <toby@tcrawley.org>
2014-07-14 07:44:28 -0600

    :refer less for clarity.

--- src/demo/scheduling.clj
+++ src/demo/scheduling.clj
@@ -1,41 +1,40 @@
 (ns demo.scheduling
-  (:require [immutant.scheduling :refer [schedule stop at in
-                                         limit every until cron]]
-            [immutant.scheduling.joda :as j]
-            [clj-time.core :as t]
-            [clj-time.periodic :as p])
+  (:require [immutant.scheduling      :as is]
+            [immutant.scheduling.joda :as ijoda]
+            [clj-time.core            :as time]
+            [clj-time.periodic        :as periodic])
   (:import java.util.Date))
 
 (def every-5s
   (or {:every [5 :seconds]}
-    (every 5 :seconds)))
+    (is/every 5 :seconds)))
 
 (def daily
-  (-> (at (or (Date.) "1830" 1395439646983))
-    (every :day)))
+  (-> (is/at (or (Date.) "1830" 1395439646983))
+    (is/every :day)))
 
 (def in-5m-until-5pm
-  (-> (in 5 :minutes)
-    (every 2 :hours, 30 :minutes)
-    (until "1700")))
+  (-> (is/in 5 :minutes)
+    (is/every 2 :hours, 30 :minutes)
+    (is/until "1700")))
 
 (def nine-am
-  (cron "0 0 9 * * ?"))
+  (is/cron "0 0 9 * * ?"))
 
 (def every-5s-cron
-  (cron "*/5 * * * * ?"))
+  (is/cron "*/5 * * * * ?"))
 
 (def every-10ms-in-500ms-4-times
-  (-> (in 500)
-    (every 10)
-    (limit 4)))
+  (-> (is/in 500)
+    (is/every 10)
+    (is/limit 4)))
 
 (defn every-3s-lazy-seq []
-  (let [at (t/plus (t/now) (t/seconds 1))
-        every (t/seconds 3)]
-    (p/periodic-seq at every)))
+  (let [at (time/plus (time/now) (time/seconds 1))
+        every (time/seconds 3)]
+    (periodic/periodic-seq at every)))
 
 (defn -main [& args]
-  (let [beep (schedule #(prn "beep") every-5s)
-        boop (j/schedule-seq #(prn "boop") (every-3s-lazy-seq))]
-    (schedule #(doall (map stop [beep boop])) (in 20 :seconds))))
+  (let [beep (is/schedule #(prn "beep") every-5s)
+        boop (ijoda/schedule-seq #(prn "boop") (every-3s-lazy-seq))]
+    (is/schedule #(doall (map is/stop [beep boop])) (is/in 20 :seconds))))
--- src/demo/web.clj
+++ src/demo/web.clj
@@ -1,37 +1,34 @@
 (ns demo.web
   (:require [clojure.pprint           :refer [pprint]]
-            [immutant.web             :refer [run]]
-            [immutant.web.javax       :refer [create-servlet]]
-            [ring.middleware.resource :refer [wrap-resource]]
-            [ring.util.response       :refer [response]]))
+            [immutant.web             :as web]
+            [immutant.web.javax       :as javax]
+            [ring.middleware.resource :as ring-resource]
+            [ring.util.response       :as ring-util]))
 
 (defn echo-request
-  "Dump the request"
+  "Echoes the request back as a string."
   [request]
-  (response (with-out-str (pprint request))))
-
-(defn world-greeter
-  "Say hello"
-  [_]
-  (response "Hello World"))
+  (ring-util/response (with-out-str (pprint request))))
 
 (def assets
   "Serve static assets from resources/public/"
-  (wrap-resource #'echo-request "public"))
+  (ring-resource/wrap-resource #'echo-request "public"))
 
 ;;; The Ring Session example: https://github.com/ring-clojure/ring/wiki/Sessions
 (defn counter [{session :session}]
   (let [count (:count session 0)
         session (assoc session :count (inc count))]
-    (-> (response (str "You accessed this page " count " times\n"))
+    (-> (ring-util/response (str "You accessed this page " count " times\n"))
         (assoc :session session))))
 
 (defn -main
   [& {:as args}]
 
-  ;; this uses an Undertow handler by default, which is faster
-  (run echo-request args)
+  ;; this uses an Undertow handler by default, which is faster than a servlet
+  (web/run echo-request args)
 
   ;; using a servlet allows the session to be shared in a WildFly
   ;; cluster and allow access to the session from websockets
-  (run (create-servlet #'counter) :path "/counter"))
+  (web/run
+    (javax/create-servlet #'counter)
+    :path "/counter"))
