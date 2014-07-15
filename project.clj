aa2adf64317a755d8bc5aa2a10ba91bb72abdd11
Toby Crawley <toby@tcrawley.org>
2014-07-15 13:34:30 -0600

    Show all of the jars piecemeal as well.

--- project.clj
+++ project.clj
@@ -5,6 +5,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.immutant/immutant "2.x.incremental.177"]
+
+                 ;; Or bring in the immutant jars piecemeal
+                 ;; [org.immutant/caching    "2.x.incremental.177"]
+                 ;; [org.immutant/messaging  "2.x.incremental.177"]
+                 ;; [org.immutant/scheduling "2.x.incremental.177"]
+                 ;; [org.immutant/web        "2.x.incremental.177"]
+
                  [org.clojure/core.memoize "0.5.6"]
                  [clj-time "0.7.0"]
                  [cheshire "5.3.1"]]
