0f3279573f6f298e31a36cd6c8b7b9ddab1f78b6
Jim Crossley <jim@crossleys.org>
2014-07-17 17:51:25 -0400

    Pick up session replication fix

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.immutant/immutant "2.x.incremental.184"]
+                 [org.immutant/immutant "2.x.incremental.185"]
 
                  ;; Or bring in the immutant jars piecemeal
-                 ;; [org.immutant/caching    "2.x.incremental.184"]
-                 ;; [org.immutant/messaging  "2.x.incremental.184"]
-                 ;; [org.immutant/scheduling "2.x.incremental.184"]
-                 ;; [org.immutant/web        "2.x.incremental.184"]
+                 ;; [org.immutant/caching    "2.x.incremental.185"]
+                 ;; [org.immutant/messaging  "2.x.incremental.185"]
+                 ;; [org.immutant/scheduling "2.x.incremental.185"]
+                 ;; [org.immutant/web        "2.x.incremental.185"]
 
                  [ring/ring-core "1.3.0"]
                  [org.clojure/core.memoize "0.5.6"]
