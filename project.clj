5afba5a0c03a60a57cc254c4240dc858da9073d0
Toby Crawley <toby@tcrawley.org>
2014-07-17 12:09:16 -0600

    Whitespace.

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.immutant/immutant "2.x.incremental.183"
-                  ]
+                 [org.immutant/immutant "2.x.incremental.183"]
 
                  ;; Or bring in the immutant jars piecemeal
                  ;; [org.immutant/caching    "2.x.incremental.183"]
