74aab7a2f87df6956948799793349525350fb1b3
Toby Crawley <toby@tcrawley.org>
2014-07-17 12:06:33 -0600

    Revert "Remove session counter example for now."
    This reverts commit f0e08f92a41ab95c8738389bfb784a93ae378efe.

--- src/demo/web.clj
+++ src/demo/web.clj
@@ -1,13 +1,34 @@
 (ns demo.web
-  (:require [clojure.pprint     :refer [pprint]]
-            [immutant.web       :as web]
-            [ring.util.response :as ring-util]))
+  (:require [clojure.pprint           :refer [pprint]]
+            [immutant.web             :as web]
+            [immutant.web.javax       :as javax]
+            [ring.middleware.resource :as ring-resource]
+            [ring.util.response       :as ring-util]))
 
 (defn echo-request
   "Echoes the request back as a string."
   [request]
   (ring-util/response (with-out-str (pprint request))))
 
+(def assets
+  "Serve static assets from resources/public/"
+  (ring-resource/wrap-resource #'echo-request "public"))
+
+;;; The Ring Session example: https://github.com/ring-clojure/ring/wiki/Sessions
+(defn counter [{session :session}]
+  (let [count (:count session 0)
+        session (assoc session :count (inc count))]
+    (-> (ring-util/response (str "You accessed this page " count " times\n"))
+        (assoc :session session))))
+
 (defn -main
   [& {:as args}]
-  (web/run echo-request args))
+
+  ;; this uses an Undertow handler by default, which is faster than a servlet
+  (web/run echo-request args)
+
+  ;; using a servlet allows the session to be shared in a WildFly
+  ;; cluster and allow access to the session from websockets
+  (web/run
+    (javax/create-servlet #'counter)
+    :path "/counter"))
