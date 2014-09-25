fcd35ead586ed97d6a2c2a0edca573fa8595232c
Toby Crawley <toby@tcrawley.org>
2014-09-25 09:45:11 -0400

    Update to latest incremental.

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.immutant/immutant "2.0.0-alpha1"]
+                 [org.immutant/immutant "2.x.incremental.307"]
 
                  ;; or bring the artifacts in piecemeal:
-                 ;; [org.immutant/caching "2.0.0-alpha1"]
-                 ;; [org.immutant/messaging "2.0.0-alpha1"]
-                 ;; [org.immutant/scheduling "2.0.0-alpha1"]
-                 ;; [org.immutant/web "2.0.0-alpha1"]
+                 ;; [org.immutant/caching "2.x.incremental.307"]
+                 ;; [org.immutant/messaging "2.x.incremental.307"]
+                 ;; [org.immutant/scheduling "2.x.incremental.307"]
+                 ;; [org.immutant/web "2.x.incremental.307"]
 
                  [compojure "1.1.8"]
                  [ring/ring-core "1.3.0"]
@@ -18,6 +18,8 @@
                  [org.clojure/core.memoize "0.5.6"]
                  [clj-time "0.7.0"]
                  [cheshire "5.3.1"]]
+  :repositories [["Immutant incremental builds"
+                  "http://downloads.immutant.org/incremental/"]]
   :plugins [[lein-immutant "2.0.0-alpha1"]]
   :main demo.core
   :uberjar-name "demo-standalone.jar"
