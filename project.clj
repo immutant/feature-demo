b28ddf352c55d6ba57fa33dd4a27dac0478dded9
Toby Crawley <toby@tcrawley.org>
2014-11-20 13:42:11 -0500

    Update to beta1.

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.immutant/immutant "2.0.0-alpha2"]
+                 [org.immutant/immutant "2.0.0-beta1"]
 
                  ;; or bring the artifacts in piecemeal:
-                 ;; [org.immutant/caching "2.0.0-alpha2"]
-                 ;; [org.immutant/messaging "2.0.0-alpha2"]
-                 ;; [org.immutant/scheduling "2.0.0-alpha2"]
-                 ;; [org.immutant/web "2.0.0-alpha2"]
+                 ;; [org.immutant/caching "2.0.0-beta1"]
+                 ;; [org.immutant/messaging "2.0.0-beta1"]
+                 ;; [org.immutant/scheduling "2.0.0-beta1"]
+                 ;; [org.immutant/web "2.0.0-beta1"]
 
                  [compojure "1.1.8"]
                  [ring/ring-core "1.3.0"]
