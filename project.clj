f140bf9d00c52b3a92cd09107933fd230dd53653
Toby Crawley <toby@tcrawley.org>
2015-04-13 14:56:44 -0400

    Update to beta3.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.immutant/immutant "2.x.incremental.561"]
+                 [org.immutant/immutant "2.0.0-beta3"]
                  [compojure "1.3.1"]
                  [ring/ring-devel "1.3.1"]
                  [org.clojure/core.memoize "0.5.6"]
@@ -15,7 +15,7 @@
                  [com.h2database/h2 "1.3.176"]]
   :repositories [["Immutant incremental builds"
                   "http://downloads.immutant.org/incremental/"]]
-  :plugins [[lein-immutant "2.0.0-beta1"]]
+  :plugins [[lein-immutant "2.0.0"]]
   :main demo.core
   :uberjar-name "demo-standalone.jar"
   :profiles {:uberjar {:aot [demo.core]}}
