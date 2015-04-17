6fc14b6186ee5a6da946c5063be49e29704eada1
Jim Crossley <jim@crossleys.org>
2015-04-17 10:26:13 -0400

    Bump to Immutant 2.0.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.immutant/immutant "2.0.0-beta3"]
+                 [org.immutant/immutant "2.0.0"]
                  [compojure "1.3.1"]
                  [ring/ring-devel "1.3.1"]
                  [org.clojure/core.memoize "0.5.6"]
