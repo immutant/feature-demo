920b9d9265fefc3a3151e3bb19c3047b809f328d
Toby Crawley <toby@tcrawley.org>
2015-07-14 15:36:20 -0400

    Update to latest immutant and clojure.

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://github.com/immutant/feature-demo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.immutant/immutant "2.0.1"]
-                 [compojure "1.3.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.immutant/immutant "2.0.2"]
+                 [compojure "1.3.4"]
                  [ring/ring-devel "1.3.1"]
                  [org.clojure/core.memoize "0.5.6"]
                  [clj-time "0.9.0"]
