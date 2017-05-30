151fb330601421a23a1bdfd71f724afcdba0900c
Toby Crawley <toby@tcrawley.org>
2017-05-30 11:29:06 -0400

    Update to 2.1.8

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.immutant/immutant "2.1.7"]
+                 [org.immutant/immutant "2.1.8"]
                  [compojure "1.4.0"]
                  [ring/ring-devel "1.3.1"]
                  [org.clojure/core.memoize "0.5.6"]
